99f8869cffbdb7929839c9fe53754c706b6d7198
LuisMR <luisorubi@hotmail.com>
2026-02-03 11:14:07 -0600

    adding return exc and definitions

---
+++ funciones/return.py
@@ -0,0 +1,22 @@
+# def mi_funcion():
+#     return "esto es una prueba"
+
+# mi_funcion()
+
+# Fijaos algo muy curioso. Si miro la terminal veo qu eno se imprime nada, porque return no imprime, solo devuelve valor, entonces, si quisiera que se imprima, debo asegurarme de asignarlo a una variable e imprimirla. Claro, porque return regresa un valor y eso me da la oprtunidad de hacer lo que quiera con ese valor. Bien pensado woody.
+
+# def mi_funcion():
+#     return "esto es una prueba"
+
+# var = mi_funcion()
+# print(var)
+
+# En este caso recordar que al asignar la función a la variable, debo hacerlo con la llamada de la función y después imprimir. 
+
+def mi_funcion2():
+    return
+
+var = mi_funcion2()
+print(var)
+
+# En este caso si no especificio el valor después de return, veremos que sale None. 
